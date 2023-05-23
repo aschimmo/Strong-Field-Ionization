5715c996174e006cfee5ee113450f5451901581c
aschimmo <115364517+aschimmo@users.noreply.github.com>
2023-05-23 14:57:53 -0400

    Update LaserPulse.m
    Added properties and instantiation to LaserPulse object

--- LaserPulse.m
+++ LaserPulse.m
@@ -1,3 +1,17 @@
 Classdef LaserPulse
 
+  properties (Constant)
+    c = 3*1e8;
+  end
+  
+  properties
+    Wavelength;
+  end
+  
+  methods
+    function obj = LaserPulse(Wavelength)
+      obj.Wavelength = Wavelength; 
+    end
+  end
+
 end
